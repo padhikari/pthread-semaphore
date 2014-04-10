5e7bea185a5b44adfbde10d55c2623d492716a9c
pabitraadhikari@gmail.com <pabitraadhikari@gmail.com>
2014-04-10 13:28:21 -0700

    semaphore initialization and thread creation

--- semaphore.c
+++ semaphore.c
@@ -10,6 +10,19 @@ sem_t mutex; //define semaphore
 int counter;
 
 int main(){
+    pthread_t thread1;
+    pthread_t thread2;
+    
+    int i[2]={0,1}; //argument to threads
+    
+    //initialize semaphore as binary semaphore
+    sem_init(&mutex, 0, 1);
+    
+    //thread creation
+    pthread_create(&thread1,NULL,(void *)&monitor,(void *)&i[0]);
+    pthread_create(&thread2,NULL,(void *)&monitor,(void *)&i[2]);
+    
+    sem_destroy(&mutex);
     
     
     exit(0);
